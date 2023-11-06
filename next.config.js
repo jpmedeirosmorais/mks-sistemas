235301012a89722e473f1dbe659bdcae6e197e32
João P M Morais <paulomedeiros5000@gmail.com>
2023-11-06 02:46:21 -0300

    fix in config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAIN],
+    domains: ["mks-sistemas.nyc3.digitaloceanspaces.com"],
   },
 };
 
